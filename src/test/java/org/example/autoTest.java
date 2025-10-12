2fe679aefedea998d8ef31511a378e8be3a4444f
Krimzon <ivanm200458@gmail.com>
2025-10-12 23:03:55 +0300

    pom update

--- src/test/java/org/example/autoTest.java
+++ src/test/java/org/example/autoTest.java
@@ -1,22 +1,18 @@
 package org.example;
 
-
 import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.ConfigurationParameters;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.core.options.Constants.*;
-import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
-        @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/feature"),
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example"),
-        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
-})
-
+@SelectClasspathResource("feature")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty"
+)
 public class autoTest {
 }
